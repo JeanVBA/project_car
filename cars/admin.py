45a65f2929f9249c6969583de04877a9cbd9279c
JeanVBA <jeanvictor999@outlook.com>
2025-01-01 08:36:48 -0300

    feature: adding the model brand and adding a photo to the car model

--- cars/admin.py
+++ cars/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
 # Register your models here.
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
+    search_fields = ['name']
+
+admin.site.register(Brand, BrandAdmin)
+
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model',)
--- cars/models.py
+++ cars/models.py
@@ -1,10 +1,23 @@
 import uuid
 from django.db import models
 
+class Brand(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 class Car(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     model = models.CharField(max_length=200)
-    brand = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.DecimalField(max_digits=19, decimal_places=4, default=0)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+
+
+    def __str__(self):
+        return self.model
